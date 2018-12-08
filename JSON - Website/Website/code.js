4478f7407c1c840e1673469b0319095e5fb76218
serdaryyy <45005433+serdaryyy@users.noreply.github.com>
2018-12-08 22:20:15 +0100

    Add files via upload

--- JSON - Website/Website/code.js
+++ JSON - Website/Website/code.js
@@ -1,26 +1,2 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
+	document.getElementById("flightnumber").innerHTML =
+	Math.floor(Math.random() * 100000001)+ "CSI";
